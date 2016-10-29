c9d4090730ebeb41f29f37b3ab5594f2f3951402
vagrant <vagrant@precise64.(none)>
2016-10-29 08:06:03 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buzzler::Application.config.secret_key_base = '5529acb6ddab912fc3afbb6b5f8ab3d437b4f39e279ce0c3030cb646c6bb43796bd1e6e3b6d9aa2b7f3aabed5b2ea9b82b53a88457498fe846e00d4b05423d11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Buzzler::Application.config.session_store :cookie_store, key: '_buzzler_session'
