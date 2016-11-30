97aa47c34519ae5a2b0b073cdf09df12c4daba65
gora4400 <gora4400.ok.721@gmail.com>
2016-11-30 19:27:27 +0900

    日本語化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,13 +16,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "ようこそバズラーへ"
+      redirect_to @user
     else
-      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
@@ -32,7 +33,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "プロフィールを更新しました"
       redirect_to @user
     else
       render 'edit'
@@ -43,7 +44,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     return redirect_to users_url if user.admin?
     user.destroy
-    flash[:success] = "User destroyed."
+    flash[:success] = "ユーザーを削除しました"
     redirect_to users_url
   end
 
