e2e0ac3289288770fe4cf89fef0338be8f9e8a82
gora4400 <gora4400.ok.721@gmail.com>
2016-12-14 00:22:12 +0900

    サンプルに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "ようこそバズラーへ"
+      flash[:success] = "ようこそサンプルへ"
       redirect_to @user
     else
       render 'new'
